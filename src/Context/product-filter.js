f970faa4ffb9d947573bf71d129934b2c4ff64a0
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-03-19 20:52:26 +0530

    [Refactor #2]: Added separate reducer

--- src/Context/product-filter.js
+++ src/Context/product-filter.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useReducer } from "react";
+import { productFilterReducer } from "../reducers";
 
 const initialState = {
   sortBy: "",
@@ -17,74 +18,6 @@ const initialState = {
 const ProductFilterContext = createContext(initialState);
 
 const useProductFilter = () => useContext(ProductFilterContext);
-
-const productFilterReducer = (state, action) => {
-  switch (action.type) {
-    case "MEN_WATCHES":
-      return {
-        ...state,
-        categories: {
-          ...state.categories,
-          menWatches: !state.categories.menWatches,
-        },
-      };
-    case "WOMEN_WATCHES":
-      return {
-        ...state,
-        categories: {
-          ...state.categories,
-          womenWatches: !state.categories.womenWatches,
-        },
-      };
-    case "BOY_WATCHES":
-      return {
-        ...state,
-        categories: {
-          ...state.categories,
-          boyWatches: !state.categories.boyWatches,
-        },
-      };
-    case "GIRL_WATCHES":
-      return {
-        ...state,
-        categories: {
-          ...state.categories,
-          girlWatches: !state.categories.girlWatches,
-        },
-      };
-    case "SET_SORT_BY":
-      return {
-        ...state,
-        sortBy: action.payload,
-      };
-    case "SET_RATING":
-      return {
-        ...state,
-        rating: action.payload,
-      };
-    case "CLEAR_FILTERS":
-      return {
-        ...initialState
-      };
-    case "SET_PRICE":
-      return {
-        ...state,
-        price: action.payload
-      };
-    case "INCLUDE_OUT_OF_STOCK":
-      return{
-        ...state,
-        includeOutOfStock: !state.includeOutOfStock
-      }
-    case "FAST_DELIVERY":
-      return{
-        ...state,
-        fastDelivery: !state.fastDelivery
-      }
-    default:
-      return state;
-  }
-};
 
 const ProductFilterProvider = ({ children }) => {
   const [state, dispatch] = useReducer(productFilterReducer, initialState);
