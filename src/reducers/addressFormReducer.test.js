f0c347c059e7d0c21582f45221178f4031cf5e71
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-12-10 19:09:11 +0530

    [Test #30]: Add test for address form reducer

---
+++ src/reducers/addressFormReducer.test.js
@@ -0,0 +1,42 @@
+import { addressFormReducer, initialState } from "./addressFormReducer";
+
+describe("addressFromReducer tests", () => {
+  it("SET_ADDRESS should set the proper value in state", () => {
+    const action = {
+      type: "SET_ADDRESS",
+      payload: { name: "country", value: "India" },
+    };
+
+    const expectedState = {
+      name: "",
+      city: "",
+      addState: "",
+      country: "India",
+      pincode: "",
+      phone: "",
+    };
+
+    const state = addressFormReducer(initialState, action);
+
+    expect(state).toEqual(expectedState);
+  });
+
+  it("SET_DUMMY_ADDRESS should set dummy address", () => {
+    const action = {
+      type: "SET_DUMMY_ADDRESS",
+    };
+
+    const expectedState = {
+      name: "Sherlock Holmes",
+      city: "Bangalore",
+      addState: "Karnataka",
+      country: "India",
+      pincode: "500099",
+      phone: "123456789",
+    };
+
+    const state = addressFormReducer(initialState, action);
+
+    expect(state).toEqual(expectedState);
+  });
+});
